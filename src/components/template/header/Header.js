366cab3e92686478d0f8326d62f436dd910f179a
subramani-barathan <bsubramani22@gmail.com>
2023-10-10 10:01:48 +0530

    Link tag has been added

--- src/components/template/header/Header.js
+++ src/components/template/header/Header.js
@@ -5,6 +5,8 @@ import ProfileImage from "../../../assets/avatars/1.png";
 import kuralJSON from "../../../json/kural.json";
 import { useState } from "react";
 import SearchList from "../../SearchList";
+import { Link } from "react-router-dom";
+
 
 function Header() {
   const [searchField, setSearchField] = useState("");
@@ -31,9 +33,9 @@ function Header() {
         id="layout-navbar"
       >
         <div className="layout-menu-toggle navbar-nav align-items-xl-center me-3 me-xl-0 d-xl-none">
-          <a className="nav-item nav-link px-0 me-xl-4" href="/">
+          <Link className="nav-item nav-link px-0 me-xl-4" href="/">
             <i className="bx bx-menu bx-sm"></i>
-          </a>
+          </Link>
         </div>
 
         <div
@@ -59,7 +61,7 @@ function Header() {
             </li>
 
             <li className="nav-item navbar-dropdown dropdown-user dropdown">
-              <a
+              <Link
                 className="nav-link dropdown-toggle hide-arrow"
                 href="/"
                 data-bs-toggle="dropdown"
@@ -71,10 +73,10 @@ function Header() {
                     className="w-px-40 h-auto rounded-circle"
                   />
                 </div>
-              </a>
+              </Link>
               <ul className="dropdown-menu dropdown-menu-end">
                 <li>
-                  <a className="dropdown-item" href="/">
+                  <Link className="dropdown-item" href="/">
                     <div className="d-flex">
                       <div className="flex-shrink-0 me-3">
                         <div className="avatar avatar-online">
@@ -90,25 +92,25 @@ function Header() {
                         <small className="text-muted">Admin</small>
                       </div>
                     </div>
-                  </a>
+                  </Link>
                 </li>
                 <li>
                   <div className="dropdown-divider"></div>
                 </li>
                 <li>
-                  <a className="dropdown-item" href="/">
+                  <Link className="dropdown-item" href="/">
                     <i className="bx bx-user me-2"></i>
                     <span className="align-middle">My Profile</span>
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a className="dropdown-item" href="/">
+                  <Link className="dropdown-item" href="/">
                     <i className="bx bx-cog me-2"></i>
                     <span className="align-middle">Settings</span>
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a className="dropdown-item" href="/">
+                  <Link className="dropdown-item" href="/">
                     <span className="d-flex align-items-center align-middle">
                       <i className="flex-shrink-0 bx bx-credit-card me-2"></i>
                       <span className="flex-grow-1 align-middle">Billing</span>
@@ -116,16 +118,16 @@ function Header() {
                         4
                       </span>
                     </span>
-                  </a>
+                  </Link>
                 </li>
                 <li>
                   <div className="dropdown-divider"></div>
                 </li>
                 <li>
-                  <a className="dropdown-item" href="auth-login-basic.html">
+                  <Link className="dropdown-item" href="/">
                     <i className="bx bx-power-off me-2"></i>
                     <span className="align-middle">Log Out</span>
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </li>
